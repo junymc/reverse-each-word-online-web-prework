47adc30248d7e1d734e181ae670eeb02d7572e38
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-06 21:45:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   array = string.split(' , ')
   new_array = []
-  string.each do |word|
-  word.reverse
+  array.each do |word|
+  new_array << word.reverse
   end
+  new_array.join(' , ')
 end
