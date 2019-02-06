dbb4945bc174def6bb14a2d25acc8e2ce6a25456
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-06 21:49:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,7 +7,7 @@
 #   new_array.join(' , ')
 # end
 
-def reverse_each_word_with_each(string)
+def reverse_each_word(string)
   # returns that same sentence with each word reversed in place.
   #First solve it using .each Then write the same method using .collect to see the difference.
   #turn the string into an array
